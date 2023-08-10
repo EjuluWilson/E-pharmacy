dcfe039df572aad6da3fb7117ea8ec8d72cf2f8a
Ejulu Wilson <emox1995@gmail.com>
2023-08-10 17:12:37 +0200

    admin addition succesfull

--- user_authentication_starter.sh
+++ user_authentication_starter.sh
@@ -10,7 +10,6 @@ get_credentials() {
     echo
 }
 
-# Function to generate a random salt using OpenSSL
 generate_salt() {
     openssl rand -hex 8
     return 0
@@ -27,10 +26,11 @@ hash_password() {
     return 0
 }
 
-# Function to check if a username already exists
-check_existing_username(){
+check_existing_username() {
     username=$1
-    grep -q "^$username:" "$credentials_file" && return 0 || return 1
+    ## verify if a username is already included in the credentials file
+    grep "^$username:" $credentials_file > /dev/null
+    return $?
 }
 
 ## function to add new credentials to the file
@@ -39,284 +39,180 @@ register_credentials() {
     # arg2 is the password
     # arg3 is the fullname of the user
     # arg4 (optional) is the role. Defaults to "normal"
-
     username=$1
     password=$2
     fullname=$3
+
     ## call the function to check if the username exists
     check_existing_username $username
     #TODO: if it exists, safely fails from the function.
-    
-    role=${4:-"normal"}
+    if [ $? -eq 0 ]; then
+        echo "Username already exists. Please choose a different username."
+        return 1
+    fi
 
-    #TODO: check if the role is valid. Should be either normal, salesperson, or admin
+    ## retrieve the role. Defaults to "normal" if the 4th argument is not passed
+    role=${4:-normal}
 
-    # first generate a salt
-    salt=$(generate_salt)
-    # then hash the password with the salt
-    hashed_pwd=$(hash_password "$password" "$salt")
-    # append the line in the specified format to the credentials file (see below)
-    echo "$username:$hashed_pwd:$salt:$fullname:$role:0" >> "$credentials_file"
+    ## check if the role is valid. Should be either normal, salesperson, or admin
+    if [ "$role" != "normal" ] && [ "$role" != "salesperson" ] && [ "$role" != "admin" ]; then
+        echo "Invalid role."
+        return 1
+    fi
+
+    ## first generate a salt
+    salt=`generate_salt`
+    ## then hash the password with the salt
+    hashed_pwd=`hash_password $password $salt`
+    ## append the line in the specified format to the credentials file (see below)
+    ## username:hash:salt:fullname:role:is_logged_in
+    echo "$username:$hashed_pwd:$salt:$fullname:$role:0" >> $credentials_file
 }
 
-# Function to verify credentials during login
+# Function to verify credentials
 verify_credentials() {
     ## arg1 is username
     ## arg2 is password
     username=$1
     password=$2
+
     ## retrieve the stored hash, and the salt from the credentials file
-    credentials_line=$(grep "^$username:" "$credentials_file")
-    if [ -z "$credentials_line" ]; then
+    # if there is no line, then return 1 and output "Invalid username"
+    line=$(grep "^$username:" $credentials_file)
+    if [ -z "$line" ]; then
         echo "Invalid username"
         return 1
     fi
-    stored_hash=$(echo "$credentials_line" | cut -d: -f2)
-    stored_salt=$(echo "$credentials_line" | cut -d: -f3)
 
     ## compute the hash based on the provided password
-    computed_hash=$(hash_password "$password" "$stored_salt")
+    stored_hash=$(echo $line | cut -d':' -f2)
+    stored_salt=$(echo $line | cut -d':' -f3)
+    computed_hash=$(hash_password $password $stored_salt)
 
     ## compare to the stored hash
-    if [ "$computed_hash" = "$stored_hash" ]; then
-        echo "$username" > .logged_in
-        sed -i "s/^$username:.*$/&:1/" "$credentials_file"
+    ### if the hashes match, update the credentials file, override the .logged_in file with the
+    ### username of the logged in user
+    if [ "$stored_hash" = "$computed_hash" ]; then
+        echo $username > .logged_in
+        sed -i "s/^\($username:[^:]*:[^:]*:[^:]*:[^:]*:\)0$/\11/" "$credentials_file"
+        role=$(echo $line | cut -d':' -f5)
+        export role
         return 0
     else
+        ### else, print "invalid password" and fail.
         echo "Invalid password"
         return 1
     fi
 }
 
-# Function to log out the current user
 logout() {
     #TODO: check that the .logged_in file is not empty
-    if [ -s .logged_in ]; then
-        # if the file exists and is not empty, read its content to retrieve the username
-        logged_in_user=$(cat .logged_in)
+    # if the file exists and is not empty, read its content to retrieve the username
+    # of the currently logged in user
+    if [ -f .logged_in ] && [ -s .logged_in ]; then
+        username=$(cat .logged_in)
         # then delete the existing .logged_in file and update the credentials file by changing the last field to 0
-        rm -f .logged_in
-        sed -i "s/^$logged_in_user:.*$/&:0/" "$credentials_file"
+        sed -i "s/^\($username:[^:]*:[^:]*:[^:]*:[^:]*:\)1$/\10/" "$credentials_file"
+        rm .logged_in
+        echo "Logged out."
     else
-        echo "No user is logged in"
+        echo "No user is currently logged in."
     fi
 }
 
-## Create the menu for the application
-echo "Welcome to the authentication system."
+# ... Main script execution, menus, and other functions ...
 
 #### BONUS
 #1. Implement a function to delete an account from the file
 
-while true; do
-    echo "1. Login"
-    echo "2. Register"
-    echo "3. Exit"
-    read -p "Choose an option: " choice
-
-    case $choice in
-        1)  get_credentials
-            verify_credentials "$user" "$pass"
-            if [ $? -eq 0 ]; then
-                echo "Logged in as $user."
-                # Provide menu for logged-in user (including admin menu if applicable)
-                while true; do
-                    echo "1. Log out"
-                    # Add more menu options here (e.g., admin functions)
-                    read -p "Choose an option: " user_option
-                    case $user_option in
-                        1)  logout
-                            echo "Logged out."
-                            break
-                            ;;
-                        # Add more cases for other menu options here
-                    esac
-                done
-            fi
-            ;;
-        2)  get_credentials
-            read -p 'Full Name: ' fullname
-            register_credentials "$user" "$pass" "$fullname"
-            if [ $? -eq 0 ]; then
-                echo "Registration successful."
-            else
-                echo "Registration failed."
-            fi
-            ;;
-        3)  echo "Exiting."
-            exit 0
-            ;;
-        *)  echo "Invalid choice. Please choose a valid option."
-            ;;
-    esac
-done#!/bin/bash
-## to be updated to match your settings
-PROJECT_HOME="."
-credentials_file="$PROJECT_HOME/data/credentials.txt"
-
-# Function to prompt for credentials
-get_credentials() {
-    read -p 'Username: ' user
-    read -rs -p 'Password: ' pass
-    echo
-}
-
-# Function to generate a random salt using OpenSSL
-generate_salt() {
-    openssl rand -hex 8
-    return 0
-}
-
-## function for hashing
-hash_password() {
-    # arg1 is the password
-    # arg2 is the salt
-    password=$1
-    salt=$2
-    # we are using the sha256 hash for this.
-    echo -n "${password}${salt}" | sha256sum | awk '{print $1}'
-    return 0
-}
-
-# Function to check if a username already exists
-check_existing_username(){
-    username=$1
-    grep -q "^$username:" "$credentials_file" && return 0 || return 1
-}
-
-## function to add new credentials to the file
-register_credentials() {
-    # arg1 is the username
-    # arg2 is the password
-    # arg3 is the fullname of the user
-    # arg4 (optional) is the role. Defaults to "normal"
-
-    username=$1
-    password=$2
-    fullname=$3
-    ## call the function to check if the username exists
-    check_existing_username $username
-    #TODO: if it exists, safely fails from the function.
-    
-    role=${4:-"normal"}
-
-    #TODO: check if the role is valid. Should be either normal, salesperson, or admin
-
-    # first generate a salt
-    salt=$(generate_salt)
-    # then hash the password with the salt
-    hashed_pwd=$(hash_password "$password" "$salt")
-    # append the line in the specified format to the credentials file (see below)
-    echo "$username:$hashed_pwd:$salt:$fullname:$role:0" >> "$credentials_file"
-}
-
-# Function to verify credentials during login
-verify_credentials() {
-    ## arg1 is username
-    ## arg2 is password
-    username=$1
-    password=$2
-    ## retrieve the stored hash, and the salt from the credentials file
-    credentials_line=$(grep "^$username:" "$credentials_file")
-    if [ -z "$credentials_line" ]; then
-        echo "Invalid username"
-        return 1
-    fi
-    stored_hash=$(echo "$credentials_line" | cut -d: -f2)
-    stored_salt=$(echo "$credentials_line" | cut -d: -f3)
-
-    ## compute the hash based on the provided password
-    computed_hash=$(hash_password "$password" "$stored_salt")
-
-    ## compare to the stored hash
-    if [ "$computed_hash" = "$stored_hash" ]; then
-        echo "$username" > .logged_in
-        sed -i "s/^$username:.*$/&:1/" "$credentials_file"
-        return 0
-    else
-        echo "Invalid password"
-        return 1
-    fi
-}
-
-# Function to log out the current user
-logout() {
-    #TODO: check that the .logged_in file is not empty
-    if [ -s .logged_in ]; then
-        # if the file exists and is not empty, read its content to retrieve the username
-        logged_in_user=$(cat .logged_in)
-        # then delete the existing .logged_in file and update the credentials file by changing the last field to 0
-        rm -f .logged_in
-        sed -i "s/^$logged_in_user:.*$/&:0/" "$credentials_file"
-    else
-        echo "No user is logged in"
-    fi
-}
-
-# Function to create an admin user
+## Function to create an admin user
 create_admin() {
     read -p 'Admin Username: ' admin_user
     read -sp 'Admin Password: ' admin_pass
     echo
     read -p 'Admin Full Name: ' admin_fullname
-    register_credentials "$admin_user" "$admin_pass" "$admin_fullname" "admin"
+    
+    # Prompt for admin credentials to allow admin creation
+    echo "Enter your admin credentials to proceed:"
+    get_credentials
+
+    # Verify admin credentials before allowing admin creation
+    verify_credentials "$user" "$pass"
+    if [ $? -eq 0 ] && [ "$role" = "admin" ]; then
+        register_credentials "$admin_user" "$admin_pass" "$admin_fullname" "admin"
+        echo "Admin created."
+    else
+        echo "Insufficient permissions. Only admin users can create admins."
+    fi
 }
 
-# Create the menu for the application
+## Main script execution starts here
 echo "Welcome to the authentication system."
 
-#### BONUS
-#1. Implement a function to delete an account from the file
-
 while true; do
-    echo "1. Login"
-    echo "2. Register"
-    echo "3. Exit"
-    read -p "Choose an option: " choice
+    if [ ! -f .logged_in ]; then
+        echo "1. Login"
+        echo "2. Register User"
+        echo "3. Exit"
+        read -p "Choose an option: " choice
 
-    case $choice in
-        1)  get_credentials
-            verify_credentials "$user" "$pass"
-            if [ $? -eq 0 ]; then
-                echo "Logged in as $user."
-                # Provide menu for logged-in user (including admin menu if applicable)
-                while true; do
-                    echo "1. Log out"
-                    if [ "$role" = "admin" ]; then
-                        echo "2. Create Admin"
-                        # Add more admin-specific options here
-                    fi
-                    read -p "Choose an option: " user_option
-                    case $user_option in
-                        1)  logout
-                            echo "Logged out."
-                            break
-                            ;;
-                        2)  if [ "$role" = "admin" ]; then
-                                create_admin
-                                echo "Admin created."
-                            else
-                                echo "You don't have permission to create an admin."
-                            fi
-                            ;;
-                        # Add more cases for other menu options here
-                    esac
-                done
-            fi
-            ;;
-        2)  get_credentials
-            read -p 'Full Name: ' fullname
-            register_credentials "$user" "$pass" "$fullname"
-            if [ $? -eq 0 ]; then
-                echo "Registration successful."
-            else
-                echo "Registration failed."
-            fi
-            ;;
-        3)  echo "Exiting."
-            exit 0
-            ;;
-        *)  echo "Invalid choice. Please choose a valid option."
-            ;;
-    esac
+        case $choice in
+            1)  # Login logic
+                get_credentials
+                verify_credentials "$user" "$pass"
+                if [ $? -eq 0 ]; then
+                    echo "Logged in as $user."
+                else
+                    echo "Login failed."
+                fi
+                ;;
+            2)  # User registration logic
+                read -p 'Username: ' username
+                read -sp 'Password: ' password
+                echo
+                read -p 'Full Name: ' fullname
+                register_credentials "$username" "$password" "$fullname"
+                echo "User registered."
+                ;;
+            3)  echo "Exiting."
+                exit 0
+                ;;
+            *)  echo "Invalid choice. Please choose a valid option."
+                ;;
+        esac
+    else
+        echo "1. Logout"
+        echo "2. Create Admin"
+        echo "3. Create Salesperson"
+        read -p "Choose an option (Admin menu): " choice
+
+        case $choice in
+            1)  # Logout logic
+                logout
+                ;;
+            2)  # Admin creation logic
+                create_admin
+                ;;
+            3)  # Salesperson creation logic
+                # Assuming the logic is similar to the admin creation
+                read -p 'Salesperson Username: ' salesperson_user
+                read -sp 'Salesperson Password: ' salesperson_pass
+                echo
+                read -p 'Salesperson Full Name: ' salesperson_fullname
+
+                echo "Enter your admin credentials to proceed:"
+                get_credentials
+
+                verify_credentials "$user" "$pass"
+                if [ $? -eq 0 ] && [ "$role" = "admin" ]; then
+                    register_credentials "$salesperson_user" "$salesperson_pass" "$salesperson_fullname" "salesperson"
+                    echo "Salesperson created."
+                else
+                    echo "Insufficient permissions. Only admin users can create salespersons."
+                fi
+                ;;
+            *)  echo "Invalid choice. Please choose a valid option."
+                ;;
+        esac
+    fi
 done
